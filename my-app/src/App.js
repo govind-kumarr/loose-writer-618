244cf8d053a6589d5a0ffc2d4ea4927261839edc
govind kumar <gk4051668@gmail.com>
2022-11-09 17:34:19 +0530

    resolved errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import './App.css';
-import AllRoutes from './AllRoutes/AllRoutes';
-=======
-
-import './App.css';
-import Footer from './Components/Footer';
-import LabPage from './Pages/LabPage';
->>>>>>> 6938454b3a8bef09db49044851a2b29697f58517
+import "./App.css";
+import AllRoutes from "./AllRoutes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <AllRoutes/>
-=======
-     
-     <LabPage />
-     <Footer />
->>>>>>> 6938454b3a8bef09db49044851a2b29697f58517
+      <AllRoutes />
     </div>
   );
 }

0417106773375d38199a79c89950ecd518e4db8f
govind kumar <gk4051668@gmail.com>
2022-11-09 13:02:02 +0530

    added routes

---
+++ my-app/src/AllRoutes/AllRoutes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SingleProduct from "../Components/SingleProduct";
+import AdminPage from "../Pages/AdminPage";
+import Cart from "../Pages/Cart";
+import Error from "../Pages/Error";
+import HomePage from "../Pages/HomePage";
+import Login from "../Pages/Login";
+import ProductPage from "../Pages/ProductPage";
+import SignUp from "../Pages/SignUp";
+
+const AllRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin-panel" element={<AdminPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products:id" element={<SingleProduct />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AllRoutes;

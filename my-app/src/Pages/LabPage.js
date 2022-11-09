a4ddf65df2573c9bd142ba7e25b898a97eb7a90f
Preetiraj3697 <satendra11kum@gmail.com>
2022-11-09 14:58:30 +0530

    add day-1

--- my-app/src/Pages/LabPage.js
+++ my-app/src/Pages/LabPage.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './LabPage.css'
 import image from '../assets/images/Poster.png'
+import Slider from './Silder'
 const LabPage = () => {
   return (
     <div>
       <div className="nav">Navbar</div>
       <div className="nav">SubNavbar</div>
         <div>
-        Labpage
-        <img src={image} alt="Poster" style={{width:'100%'}}/>
+        <Slider />
+        <img src={image} alt="Poster" className='Poster' />
         </div>  
     </div>
   )

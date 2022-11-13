ef7cb1607a83f23bef70a61fee4125257ec6095f
govind kumar <gk4051668@gmail.com>
2022-11-13 14:59:15 +0530

    added mukuls code

--- my-app/src/Redux/store.js
+++ my-app/src/Redux/store.js
@@ -1,4 +1,4 @@
-import {legacy_createStore, applyMiddleware, compose, combineReducers  } from "redux"
+import {legacy_createStore, applyMiddleware, compose, combineReducers  } from "redux" 
 import thunk from "redux-thunk"
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
 import {reducer as AppReducer} from "./AppReducer/reducer"
